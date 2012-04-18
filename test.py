2bc7e97ab32c7e38086d4d35477e17b508f1ff2f
Luke Macken <lmacken@redhat.com>
2012-04-17 20:08:49 -0400

    Their API now returns integers for size/length

--- test.py
+++ test.py
@@ -6,24 +6,24 @@ class TestQuantumRandom(unittest.TestCase):
     def test_uint16_json_api(self):
         data = quantumrandom.get_data('uint16', 1, 1)
         assert data['success']
-        assert int(data['length']) == 1
+        assert data['length'] == 1
         assert data['type'] == 'uint16'
         assert len(data['data']) == 1
 
     def test_uint16_json_api_max_array(self):
         data = quantumrandom.get_data('uint16', 100, 100)
         assert data['success']
-        assert int(data['length']) == 100
+        assert data['length'] == 100
         assert data['type'] == 'uint16'
         assert len(data['data']) == 100
 
     def test_hex16_json_api(self):
         data = quantumrandom.get_data('hex16', 1, 1)
         assert data['success']
-        assert int(data['length']) == 1
+        assert data['length'] == 1
         assert data['type'] == 'string'
         assert len(data['data']) == 1
-        assert int(data['size']) == 1
+        assert data['size'] == 1
 
     def test_uint16_json_api_long_array(self):
         try:
