eb36b7dd1347ec2c3a7e480001d6700d95f7a876
Luke Macken <lmacken@redhat.com>
2013-03-04 07:57:47 -0500

    Update the copyright

--- quantumrandom/dev.py
+++ quantumrandom/dev.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 Luke Macken <lmacken@redhat.com>
+# Copyright (c) 2012-2013 Luke Macken <lmacken@redhat.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
